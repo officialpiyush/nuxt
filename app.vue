05018a090f37b4a8f77a3df2ceb3458ba38299b2
Piyush <bhangalepiyush@gmail.com>
2023-05-26 19:39:20 +0530

    feat: use custom element instead of nuxt-welcome

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ import '@unocss/reset/tailwind.css'
 </script>
 
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-screen bg-zinc-900 flex items-center justify-center text-white font-mono">
+    Hello, Pizza!
   </div>
 </template>

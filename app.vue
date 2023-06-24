05837a747be1c9201fec0fafcbc37327c3ba5b2e
Piyush <bhangalepiyush@gmail.com>
2023-06-24 09:54:15 +0530

    feat: add file based routing

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="h-screen flex items-center justify-center bg-zinc-900 font-mono text-white">
-    Hello, Pizza!
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>

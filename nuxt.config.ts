d8617e15c9aabcd5f39859979f03c9fbf5c2667b
Piyush <bhangalepiyush@gmail.com>
2023-06-29 09:44:07 +0530

    chore(nuxt): add toaster css to imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@vueuse/nuxt', '@unocss/nuxt'],
-  css: ['@unocss/reset/tailwind.css'],
+  css: [
+    '@unocss/reset/tailwind.css',
+    'vue-toast-notification/dist/theme-sugar.css',
+  ],
   imports: {
     imports: [
       {

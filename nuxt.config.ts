8e5d92808ff554f7e71ff785b31457607c4ed015
Piyush <bhangalepiyush@gmail.com>
2023-06-30 19:10:08 +0530

    chore(nuxt): enable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
   modules: ['@vueuse/nuxt', '@unocss/nuxt'],
   css: [
     '@unocss/reset/tailwind.css',

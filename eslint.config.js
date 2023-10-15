6657808930e7cd3bbdaea47a34ecc982b7d9d3aa
Piyush <bhangalepiyush@gmail.com>
2023-10-15 11:28:39 +0530

    chore(eslint): migrate to flat config

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+import unoCssPlugin from '@unocss/eslint-config'
+
+export default antfu({
+  files: ['**/*.{vue,js,ts}'],
+  vue: true,
+  typescript: true,
+  plugins: {
+    unocss: unoCssPlugin,
+  },
+})

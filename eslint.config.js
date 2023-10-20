353f6f3830049d6e67c37587df9a3c9216949e8a
Piyush <bhangalepiyush@gmail.com>
2023-10-21 02:25:47 +0530

    fix(config/eslint): scan for all files in the workspace root

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import antfu from '@antfu/eslint-config'
 import unoCssPlugin from '@unocss/eslint-config'
 
 export default antfu({
-  files: ['**/*.{vue,js,ts}'],
+  files: ['./**/*.{vue,js,ts}'],
   vue: true,
   typescript: true,
   plugins: {

6fc80925a5be93ac428e1850bb39d799f227111f
Piyush <bhangalepiyush@gmail.com>
2023-10-21 10:45:45 +0530

    chore(nuxt): add sidebase auth to modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   build: {
     transpile: ['trpc-nuxt'],
   },
-  modules: ['@vueuse/nuxt', '@unocss/nuxt'],
+  modules: ['@vueuse/nuxt', '@unocss/nuxt', '@sidebase/nuxt-auth'],
   css: [
     '@unocss/reset/tailwind.css',
     'notivue/notifications.css',
